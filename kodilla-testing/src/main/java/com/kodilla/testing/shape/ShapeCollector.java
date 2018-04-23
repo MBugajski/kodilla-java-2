062689135fc1295f6c42bef4064db670ff312699
MBugajski <michalbugajski@outlook.com>
2018-04-23 13:06:53 +0200

    Created ShapeCollector class without implementation

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+
+public class ShapeCollector {
+    private ArrayList<Shape> collection = new ArrayList<>();
+
+    public void addFigure(Shape shape) {
+
+    }
+
+    public int collectionSize() {
+        return 100;
+    }
+
+    public boolean removeFigure(Shape shape) {
+
+        return true;
+    }
+
+    public String getFigure(int n) {
+
+        return null;
+    }
+
+
+    public void showFigures() {
+
+    }
+
+}
