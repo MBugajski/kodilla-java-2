47da177eca5220d5cc29ab8eabcc5f5f254e8151
MBugajski <michalbugajski@outlook.com>
2018-04-23 21:27:06 +0200

    Created a StatisticianTestSuite for Statistician class using Mockito.mock()

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticianTestSuite.java
@@ -0,0 +1,174 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StatisticianTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of the tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testZeroPosts() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(0);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double commentsPerPost = statistician.commentsPerPost;
+
+        //Then
+        Assert.assertEquals(-1, commentsPerPost, 0.0001);
+    }
+
+    @Test
+    public void testThousandPosts() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(String.valueOf(i));
+        }
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double postsPerUser = statistician.postsPerUser;
+
+        //Then
+        Assert.assertEquals(100, postsPerUser, 0.0001);
+    }
+
+    @Test
+    public void testZeroComments() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(String.valueOf(i));
+        }
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double commentsPerUser = statistician.commentsPerUser;
+
+        //Then
+        Assert.assertEquals(0, commentsPerUser, 0.0001);
+    }
+
+
+    @Test
+    public void testMorePosts() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(50);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double commentsPerPost = statistician.commentsPerPost;
+
+        //Then
+        Assert.assertEquals(0.2, commentsPerPost, 0.0001);
+    }
+
+    @Test
+    public void testMoreComments() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(50);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double commentsPerPost = statistician.commentsPerPost;
+
+        //Then
+        Assert.assertEquals(5, commentsPerPost, 0.0001);
+    }
+
+    @Test
+    public void testZeroUsers() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> users = new ArrayList<>();
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double commentsPerUser = statistician.commentsPerUser;
+
+        //Then
+        Assert.assertEquals(-1, commentsPerUser, 0.0001);
+    }
+
+    @Test
+    public void testHundredUsers() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> users = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            users.add(String.valueOf(i));
+        }
+        when(statisticsMock.usersNames()).thenReturn(users);
+        when(statisticsMock.postsCount()).thenReturn(10);
+
+
+        Statistician statistician = new Statistician();
+        statistician.calculateAdvStatistics(statisticsMock);
+
+        //When
+        double postsPerUser = statistician.postsPerUser;
+
+        //Then
+        Assert.assertEquals(0.1, postsPerUser, 0.0001);
+    }
+
+}
+
