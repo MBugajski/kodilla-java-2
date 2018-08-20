225174ae0a558f5d887b02c81d24d3c468ab6183
MBugajski <>
2018-08-20 20:59:34 +0200

    Module 13 Hibernate (submodulses 1, 2)

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,78 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table (name = "TASKS")
+public class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+
+    public Task() {
+    }
+
+    public Task(String description, int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column (name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column (name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @NotNull
+    @Column (name = "CREATED")
+    public Date getCreated() {
+        return created;
+    }
+    @Column (name = "DURATION")
+    public int getDuration() {
+        return duration;
+    }
+    @OneToOne (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    private void setCreated(Date created) {
+        this.created = created;
+    }
+
+    private void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+}
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,54 @@
+package com.kodilla.hibernate.tasklist;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table (name = "TASKLIST")
+public class TaskList {
+    private int id;
+    private String listName;
+    private String description;
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column (name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column (name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column (name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+}
